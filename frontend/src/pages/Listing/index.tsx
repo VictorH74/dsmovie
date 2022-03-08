bb23c62e06dccd354fa3407543efff76107e6adb
Victor Almeida <victorh.almeida7@gmail.com>
2022-03-08 20:11:27 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,40 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 
-function Listing(){
-    return(
-        <Pagination/>
+// ""col-sm-6 col-lg-4 col-xl-3"" -> break points do grid do bootstrap
+// "mb-4 = margin-bottom"
+
+function Listing() {
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                </div>
+                
+
+            </div>
+
+
+        </>
+
     );
 }
 

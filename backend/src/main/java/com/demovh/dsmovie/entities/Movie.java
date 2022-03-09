57f7750a961ff8a1bd1d4da8927b17d26930b517
Victor Almeida <victorh.almeida7@gmail.com>
2022-03-09 20:17:10 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/demovh/dsmovie/entities/Movie.java
@@ -0,0 +1,93 @@
+package com.demovh.dsmovie.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie") // -> DEFINIR NOME DA TABELA NO BANCO DE DADOS
+public class Movie{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // -> GERAR ID AUTOMÁTICO NO DATABASE
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+	
+	public Movie() {
+		
+	}
+
+	public Movie(Long id, String title, Double score, Integer count, String image) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.score = score;
+		this.count = count;
+		this.image = image;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+	
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}

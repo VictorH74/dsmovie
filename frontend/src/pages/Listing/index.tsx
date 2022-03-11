6acbc8b2b55f86f0b687eb1d1951ba1608fd2bea
Victor Almeida <victorh.almeida7@gmail.com>
2022-03-11 16:12:27 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -26,43 +26,47 @@ function Listing() {
            [NUMERO DA PAG, FUNÇÃO QUE ALTERA O ESTADO] = useState(VALOR INICIAL DA PAG.) */
     const [pageNumber, setPageNumber] = useState(0);
 
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+
     useEffect(() => {
 
-         axios.get(`${BASE_URL}/movies?size=12&page=0`)
+         axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`) // "&sort=id" ORDENAR BUSCA POR ID
             .then(response  => { // COMANDO A SER EXECUTADO DEPOIS QUE A REQUEST GET RETORNAR UM OBJ
                 const data = response.data as MoviePage;
-                console.log(data);
-                setPageNumber(data.number);
+                setPage(data)
             });
 
-    }, []);
-
-   
-
+    }, [pageNumber]); // RODAR O COMANDO ACIMA QUANDO O VALOR DE "pageNumber" FOR ALTERADO
 
 
     return (
         <>
-            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
 
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <MovieCard />
-                    </div>
+
+                    {page.content.map(item => ( // ".map()" EXECUTA UMA FUNÇÃO PARA CADA ITEM DOS DADOS EM "page.content"
+
+                    // "Em uma renderização dinâmica de coleção, cada elemento renderizado DEVE possuir um atributo key"
+
+                        <div key={item.id} className="col-sm-6 col-lg-4 col-xl-3 mb-4">
+                            <MovieCard movie = {item}/>
+                        </div>
+
+                        )
+                    )}
+
                 </div>
                 
 

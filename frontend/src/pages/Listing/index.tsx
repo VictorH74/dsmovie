9a0d2b09fc62c4fc084417897c9cc8322aefb598
Victor Almeida <victorh.almeida7@gmail.com>
2022-03-11 15:40:48 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 
@@ -9,15 +11,39 @@ import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response  => { // COMANDO A SER EXECUTADO DEPOIS QUE A REQUEST GET RETORNAR UM OBJ
-            console.log(response.data);
-        }) 
+/*  REACTS HOOKS: useState E useEffect -> Hooks são funções cujo comportamento está vinculado ao estado e
+                  ao ciclo de vida do React a partir de componentes funcionais.
+
+    CONTROLAR CICLO DE VIDA DO COMPONENTE
+
+    Hook: useState -> Manter estado no componente
+    Hook: useEffect -> Executar algo na instanciação ou destruição do componente, observar estado
+
+    https://pt-br.reactjs.org/docs/hooks-overview.html -> MAIS INFORMAÇÕES
+*/
+
+/*  DEFININDO UM useState
+           [NUMERO DA PAG, FUNÇÃO QUE ALTERA O ESTADO] = useState(VALOR INICIAL DA PAG.) */
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+
+         axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response  => { // COMANDO A SER EXECUTADO DEPOIS QUE A REQUEST GET RETORNAR UM OBJ
+                const data = response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
+            });
+
+    }, []);
+
+   
+
 
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
 
